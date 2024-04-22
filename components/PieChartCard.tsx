23a8c04b8fe26b646669d3038f9f042d02b5e4e1
PeterG <p.goraj0@gmail.com>
2024-04-22 22:12:04 +0200

    data values shown

--- components/PieChartCard.tsx
+++ components/PieChartCard.tsx
@@ -1,4 +1,11 @@
-import { StyleSheet, View, Text, Dimensions, TextStyle } from 'react-native';
+import {
+  StyleSheet,
+  View,
+  Text,
+  Dimensions,
+  TextStyle,
+  Pressable,
+} from 'react-native';
 import { PieChart } from 'react-native-chart-kit';
 import { AbstractChartConfig } from 'react-native-chart-kit/dist/AbstractChart';
 
@@ -6,6 +13,8 @@ import { ColorIntensity, LegendProps, PieChartDataProps } from '../utils/types';
 import usePrimaryColors from '../hooks/usePrimaryColors';
 import Legend from './Legend';
 import DetailsCard from './ui/cards/DetailsCard';
+import { useState } from 'react';
+import colors from '../utils/colors';
 
 interface PieChartCardProps {
   title: string;
@@ -23,6 +32,8 @@ export default function PieChartCard({
   cardColor,
 }: PieChartCardProps) {
   const color = usePrimaryColors(cardColor.color);
+
+  const [isValuesShown, setIsValuesShown] = useState<boolean>(false);
 
   const chartConfig: AbstractChartConfig = {
     backgroundGradientFrom: '#1E2923',
@@ -42,21 +53,50 @@ export default function PieChartCard({
       title={title}
       titlePosition={titlePosition}
     >
-      <View style={legendData ? styles.withLegend : {}}>
-        <PieChart
-          data={data}
-          width={legendData ? 320 : 400}
-          height={legendData ? 240 : 280}
-          chartConfig={chartConfig}
-          accessor={'value'}
-          backgroundColor={'transparent'}
-          paddingLeft={'0'}
-          center={[legendData ? 80 : 12, 0]}
-          hasLegend={legendData ? false : true}
-        />
+      <>
+        <Pressable
+          onPressIn={() => setIsValuesShown(true)}
+          onPressOut={() => setIsValuesShown(false)}
+        >
+          <View style={legendData ? styles.withLegend : {}}>
+            <PieChart
+              data={data}
+              width={legendData ? 320 : 400}
+              height={legendData ? 240 : 280}
+              chartConfig={chartConfig}
+              accessor={'value'}
+              backgroundColor={'transparent'}
+              paddingLeft={'0'}
+              center={[legendData ? 80 : 12, 0]}
+              hasLegend={legendData ? false : true}
+            />
 
-        {legendData && <Legend legendData={legendData} />}
-      </View>
+            {legendData && <Legend legendData={legendData} />}
+          </View>
+        </Pressable>
+
+        {isValuesShown && (
+          <View style={styles.valuesContainer}>
+            {data.map((data) => (
+              <View style={styles.valueWrapper}>
+                <View
+                  style={[styles.valueDot, { backgroundColor: data.color }]}
+                />
+                <Text style={styles.valueText}>{data.name}</Text>
+                <Text style={styles.valueText}>{`${data.value.toFixed(
+                  2
+                )} PLN`}</Text>
+              </View>
+            ))}
+            <View style={styles.valueWrapper}>
+              <Text style={styles.valueText}>All costs</Text>
+              <Text style={styles.valueText}>{`${data
+                .reduce((acc, expense) => acc + expense.value, 0)
+                .toFixed(2)} PLN`}</Text>
+            </View>
+          </View>
+        )}
+      </>
     </DetailsCard>
   );
 }
@@ -65,5 +105,35 @@ const styles = StyleSheet.create({
   withLegend: {
     justifyContent: 'center',
     alignItems: 'center',
+  },
+  valuesContainer: {
+    position: 'absolute',
+    justifyContent: 'center',
+    alignItems: 'center',
+
+    width: '104%',
+    height: '102%',
+
+    backgroundColor: '#0000006a',
+    borderRadius: 16,
+  },
+  valueWrapper: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    width: 200,
+    padding: 4,
+    margin: 4,
+
+    backgroundColor: colors.grey[200],
+    borderRadius: 16,
+  },
+  valueDot: {
+    width: 16,
+    height: 16,
+    borderRadius: 8,
+  },
+  valueText: {
+    color: colors.fontDark,
   },
 });
